3d80ce1180fd5edcc20c390235b9f11cbb173c7a
Chinmay <chaudhari.chinmay12345@gmail.com>
2024-10-25 18:05:33 +0530

    Jay Jhulelal 💦

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,10 +6,10 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
+
         <WalletContractProvider>
             <App />
             <ToastContainer position="top-right" autoClose={3000} hideProgressBar={false} />
         </WalletContractProvider>
-    </React.StrictMode>
+
 );
