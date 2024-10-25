d9911070699d292b46d955aebc745587ad067c1f
Dhairyash Jain <jaindhairyashj@gmail.com>
2024-10-25 11:37:42 +0530

    ititial setup

---
+++ client/src/Constants.js
@@ -0,0 +1,141 @@
+const abi=[
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "addre",
+          "type": "address"
+        },
+        {
+          "internalType": "string",
+          "name": "name",
+          "type": "string"
+        },
+        {
+          "internalType": "uint256",
+          "name": "age",
+          "type": "uint256"
+        },
+        {
+          "internalType": "string",
+          "name": "city",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "addr",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "bank_account",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "adhar_num",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "image",
+          "type": "string"
+        }
+      ],
+      "name": "addKYC",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "person",
+          "type": "address"
+        }
+      ],
+      "name": "approveKYC",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "user",
+          "type": "address"
+        }
+      ],
+      "name": "getKYC",
+      "outputs": [
+        {
+          "components": [
+            {
+              "internalType": "string",
+              "name": "name",
+              "type": "string"
+            },
+            {
+              "internalType": "uint256",
+              "name": "age",
+              "type": "uint256"
+            },
+            {
+              "internalType": "string",
+              "name": "city",
+              "type": "string"
+            },
+            {
+              "internalType": "string",
+              "name": "addr",
+              "type": "string"
+            },
+            {
+              "internalType": "string",
+              "name": "bank_account",
+              "type": "string"
+            },
+            {
+              "internalType": "string",
+              "name": "adhar_num",
+              "type": "string"
+            },
+            {
+              "internalType": "string",
+              "name": "image",
+              "type": "string"
+            },
+            {
+              "internalType": "enum KYC.Status",
+              "name": "st",
+              "type": "uint8"
+            }
+          ],
+          "internalType": "struct KYC.Person",
+          "name": "",
+          "type": "tuple"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "person",
+          "type": "address"
+        }
+      ],
+      "name": "rejectKYC",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    }
+  ]
+
+const contract="0xd730BE197f5F5DAD0FD1F67B7B2a0bF5df9856d7"
+
+export default{abi, contract}
---
+++ client/src/Context/WalletProvider.jsx
@@ -0,0 +1,43 @@
+import React, { createContext, useContext, useState, useEffect } from 'react';
+import {abi, contract as contractAddress} from "../Constants"
+import { ethers } from 'ethers';
+
+const WalletContractContext = createContext();
+
+export const WalletContractProvider = ({ children }) => {
+    const [walletAddress, setWalletAddress] = useState(null);
+    const [contract, setContract] = useState(null);
+    const connectWallet = async () => {
+        if (window.ethereum) {
+            try {
+                await window.ethereum.request({ method: 'eth_requestAccounts' });
+                const provider = new ethers.BrowserProvider(window.ethereum);
+                const signer = provider.getSigner();
+                const address = await signer.getAddress();
+                setWalletAddress(address);
+                initializeContract(signer);
+            } catch (error) {
+                console.error("Wallet connection failed:", error);
+            }
+        } else {
+            console.error("MetaMask not found. Please install it.");
+        }
+    };
+
+    const initializeContract = (signer) => {
+        const contractInstance = new ethers.Contract(contractAddress, abi, signer);
+        setContract(contractInstance);
+    };
+
+    useEffect(() => {
+        connectWallet();
+    }, []);
+
+    return (
+        <WalletContractContext.Provider value={{ walletAddress, contract, connectWallet }}>
+            {children}
+        </WalletContractContext.Provider>
+    );
+};
+
+export const useWalletContract = () => useContext(WalletContractContext);
